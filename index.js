0ea7a128afbff6e3d55b297733be26716aaf0faf
AlexeiDeepIA <alexeipalacios@deepia.mx>
2024-08-06 12:55:36 -0600

    cors

--- index.js
+++ index.js
@@ -22,19 +22,10 @@ const RefillRoutes = require('./routes/RefillRoutes.js')
 const app = express();
 const port = process.env.PORT || 3000;
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Aquí puedes especificar los orígenes permitidos
-    const allowedOrigins = ['http://localhost:4200', 'https://tudominio.com'];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200
+  origin: '*', // Permite cualquier origen
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Permite todos los métodos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Permite estos encabezados
+  optionsSuccessStatus: 200 // Para navegadores antiguos que no admiten CORS
 };
 
 app.use(cors(corsOptions));
