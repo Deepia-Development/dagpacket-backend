4cb389f77e8f2b70e90ce28eba61809802adf0a6
Oz4ru <ganthud@gmail.com>
2024-09-15 13:13:34 -0600

    barcode scan

--- services/ScanService.js
+++ services/ScanService.js
@@ -1,18 +1,19 @@
+const Scan = require('../models/scanModel');
 
-  const Scan = require('../models/scanModel');
-
-  const scanService = {
-    async getAllScan() {
-      try {
-        return await Scan.find();  
-      } catch (error) {
-        console.error('Error al obtener los escaneos:', error);
-        throw new Error('Error al obtener los escaneos');
-      }
+const scanService = {
+  async getAllScan() {
+    try {
+      return await Scan.find();
+    } catch (error) {
+      console.error('Error al obtener los escaneos:', error);
+      throw new Error('Error al obtener los escaneos');
     }
-  };
-  
-  module.exports = scanService;
-  
+  },
 
+  // NUEVO: Servicio para actualizar el código de barras
+  async updateBarcode(id, barcode) {
+    return await Scan.findByIdAndUpdate(id, { barcode: barcode }, { new: true });
+  }
+};
 
+module.exports = scanService;
