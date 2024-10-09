75d201565f810543bfd4f9bb2d3c1f5881bc72f3
Oz4ru <ganthud@gmail.com>
2024-10-09 12:19:49 -0600

    diseño

--- routes/scanRoutes.js
+++ routes/scanRoutes.js
@@ -3,10 +3,13 @@ const scanController = require('../controllers/scanController');
 
 const router = express.Router();
 
-
+// Obtener todos los escaneos
 router.get('/scans', scanController.getAllScans);
 
-
+// Actualizar un código de barras existente
 router.put('/scans/:id', scanController.updateBarcode);
+
+// Agregar un nuevo código de barras
+router.post('/scans', scanController.createBarcode);  // NUEVA RUTA POST
 
 module.exports = router;
